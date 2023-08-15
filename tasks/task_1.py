d264c32b71c2914cfe39f5d599f6307dae4efa3c
Sem Salimov <salimovsemen43@gmail.com>
2023-08-15 22:11:22 +0600

    merge

--- tasks/task_1.py
+++ tasks/task_1.py
@@ -3,5 +3,4 @@ num_2: int = int(input('Введите второе число: '))
 
 average: float = (num_1 + num_2) / 2
 
-average = (num_1 + num_2) / 2  
-print ('Средние арифметическое чисел', num_1, 'и', num_2, 'равно', average) 
+print('Средние арифметическое чисел', num_1, 'и', num_2, 'равно', average)
